380cd42bd8838d3ace447e31838f8ed2d09c7209
Seranged <80223622+Seranged@users.noreply.github.com>
2026-04-09 19:08:33 +0100

    fix: prevent action button labels from wrapping on narrow screens

--- components/entities/portfolio/PortfolioSavingItem.vue
+++ components/entities/portfolio/PortfolioSavingItem.vue
@@ -200,7 +200,7 @@ const onClick = () => {
           </div>
         </div>
         <div
-          class="flex items-center gap-8"
+          class="flex items-center gap-8 [&>*]:whitespace-nowrap"
           @click.stop
         >
           <UiButton
@@ -331,7 +331,7 @@ const onClick = () => {
           </div>
         </div>
         <div
-          class="flex items-center gap-8"
+          class="flex items-center gap-8 [&>*]:whitespace-nowrap"
           @click.stop
         >
           <UiButton
