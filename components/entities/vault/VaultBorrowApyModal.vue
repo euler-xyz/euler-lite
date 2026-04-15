72c3a54201e1f3ca1f3406c82149bd23e7adf223
Seranged <80223622+Seranged@users.noreply.github.com>
2026-04-15 19:33:40 +0100

    fix: add white background to reward provider logos for contrast
    Reward provider logos (Merkl, Brevis) use dark-on-transparent designs
    that blend into the dark modal surfaces, leaving them effectively
    invisible. Apply a consistent white backing with subtle rounding and
    padding to the provider logo frames so any provider logo is legible
    on dark backgrounds.

--- components/entities/vault/VaultBorrowApyModal.vue
+++ components/entities/vault/VaultBorrowApyModal.vue
@@ -138,13 +138,13 @@ const handleClose = () => {
             ><img
               v-if="PROVIDER_LOGOS[reward.source]"
               :src="PROVIDER_LOGOS[reward.source]"
-              class="w-14 h-14 inline-block align-middle mr-2"
+              class="w-14 h-14 inline-block align-middle mr-2 bg-white rounded-sm p-1"
               :alt="PROVIDER_LABELS[reward.source]"
             >{{ PROVIDER_LABELS[reward.source] || reward.source }}</a><template v-else>
               <img
                 v-if="PROVIDER_LOGOS[reward.source]"
                 :src="PROVIDER_LOGOS[reward.source]"
-                class="w-14 h-14 inline-block align-middle mr-2"
+                class="w-14 h-14 inline-block align-middle mr-2 bg-white rounded-sm p-1"
                 :alt="PROVIDER_LABELS[reward.source]"
               >{{ PROVIDER_LABELS[reward.source] || reward.source }}
             </template>{{ reward.isCollateralSpecific ? ', collateral bonus' : '' }}{{ reward.endDate ? `, ends ${reward.endDate.toFormat('MMMM dd, yyyy')}` : '' }})
